bd7c2afd16292c17bb76f7bb6c7553359f16b583
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-09-11 20:44:44 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,7 @@ def valid_move?(board, index)
   if index > 8
     return false
   else
-    if position_taken? == false
+    if position_taken?(board, index) == false
       return true
     else
       return false
